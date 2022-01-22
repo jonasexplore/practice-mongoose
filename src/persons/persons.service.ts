10e54acff96205cc74ae007bf97ae4a151976c30
Jonas Brito <jonasexplore@gmail.com>
2022-01-22 20:54:13 -0300

    feat(person): create module

---
+++ src/persons/persons.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Person } from './domain/person';
+
+@Injectable()
+export class PersonsService {
+  constructor(
+    @InjectModel('person')
+    private readonly personModel: Model<Person>,
+  ) {}
+
+  async create(createPersonDto: Person) {
+    const createdPerson = new this.personModel(createPersonDto);
+    return createdPerson.save();
+  }
+
+  async findAll() {
+    return this.personModel.find();
+  }
+
+  async findOne(id: string) {
+    return this.personModel.findById(id);
+  }
+
+  async update(id: string, updatePersonDto: Person) {
+    return this.personModel.updateOne({ _id: id }, updatePersonDto);
+  }
+
+  async remove(id: string) {
+    return this.personModel.deleteOne({ _id: id });
+  }
+}
